b2a2d88789fbaaf53f4995d3e288516ba4ef8a9a
xiaoma555 <2637144005@qq.com>
2024-08-06 21:06:12 +0800

    chang: 修改git忽略目录IP地址

--- tauri-app/vite.config.ts
+++ tauri-app/vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig(async () => ({
     proxy: {
       "/api": {
         // target: "http://192.168.2.19:8080/api",// 公司运行IP
-        target: "http://192.168.0.118:8080/api",// 家里运行IP
+        // target: "http://192.168.0.118:8080/api",// 家里运行IP
+        target: "http://192.168.1.6:8080/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
